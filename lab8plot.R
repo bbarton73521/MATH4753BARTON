831a57c038fe53d2fe3c7c270856b3cf02211aca
bbarton73521 <78321621+bbarton73521@users.noreply.github.com>
2021-03-29 14:47:26 -0500

    Add files via upload

---
+++ lab8plot.R
@@ -0,0 +1,18 @@
+#' @Title Central Limit Theorem
+#'
+#' @param n a quantitative vector
+#' @param iter number of iterations
+#'
+#' @return a histogram
+#' @export
+#'
+#' @examples myclt(10, 10000)
+#' \dontrun{(sm)}
+myclt=function(n,iter){
+  y=runif(n*iter,0,5) # A
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE) #B
+  sm=apply(data,2,sum) #C
+  hist(sm)
+  sm
+}
+
